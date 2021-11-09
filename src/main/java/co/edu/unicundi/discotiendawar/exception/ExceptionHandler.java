7ee954223d652575bb0386451bbbf6061c835f9b
jdalzate0204 <james.alzate22@hotmail.com>
2021-11-09 18:38:37 -0500

    Se realizó crear album

--- src/main/java/co/edu/unicundi/discotiendawar/exception/ExceptionHandler.java
+++ src/main/java/co/edu/unicundi/discotiendawar/exception/ExceptionHandler.java
@@ -1,35 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.unicundi.discotiendawar.exception;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import co.edu.unicundi.discotiendajar.exception.ResourceIllegalArgumentException;
+import javax.ws.rs.core.*;
+import javax.ws.rs.ext.*;
 
 /**
  *
- * @author PAULA GUZMAN
+ * @author acer
  */
 @Provider
-public class ExceptionHandler implements ExceptionMapper<Exception>{
-@Context UriInfo uriInfo;
-
-    @Override
-    public Response toResponse(Exception ex) {
-    ex.printStackTrace();
-    ExceptionWraper ew;
+public class ExceptionHandler implements ExceptionMapper<Exception> {
+    @Context private UriInfo uriInfo;
     
-    if (ex instanceof ArrayIndexOutOfBoundsException) {
-    ew = new ExceptionWraper(Response.Status.BAD_REQUEST.getStatusCode(), Response.Status.BAD_REQUEST.getReasonPhrase(), ex.getMessage(), uriInfo.getPath());
-    return Response.status(Response.Status.BAD_REQUEST).entity(ew).build();
-    } else {
-    ew = new ExceptionWraper(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), "", uriInfo.getPath());
-    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ew).build();
+    @Override
+    public Response toResponse(Exception e) {
+        e.printStackTrace();
+        ExceptionWraper ew;
+        
+        if (e instanceof ResourceIllegalArgumentException ) { //400
+            ew = new ExceptionWraper(Response.Status.BAD_REQUEST.getStatusCode(), 
+                                      Response.Status.BAD_REQUEST.getReasonPhrase(), 
+                                      e.getMessage(), 
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.BAD_REQUEST).entity(ew).build();  
+        } else {
+            ew = new ExceptionWraper(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), 
+                                      Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), 
+                                      "", 
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ew).build();
+        }
     }
-   }
+    
 }
